dbcd15a335632d4f3a5b2a07b39a685ca63eba58
gregory <gregorykimbira7@gmail.com>
2025-06-14 20:03:19 +0300

    created the main app class

--- navigation/lib/main.dart
+++ navigation/lib/main.dart
@@ -4,12 +4,18 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+  @override
+  _MainAppState createState() => _MainAppState();
 
+}
+
+class _MainAppState extends State<MainApp>{
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
