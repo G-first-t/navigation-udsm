4cb925eda1898b0dc566959d1ea76b57d48c0bd3
gregory <gregorykimbira7@gmail.com>
2025-06-27 13:13:24 +0300

    adding firebase

--- navigation/lib/main.dart
+++ navigation/lib/main.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:navigation/pages/home_screen.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:navigation/utils/logger.dart';
 
-
-void main() {
-  runApp(const MainApp());
+ //The main function or the root for our application
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  AppLogger.info('Starting application initialization...');
+  try {
+    // Initializing Firebase
+    AppLogger.debug('Initializing Firebase...');
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    AppLogger.info('Firebase initialized successfully');
+    // Running the application
+    runApp(const MainApp());
+    AppLogger.info('Running the app...');
+  } catch (e, stack) {
+    AppLogger.error('Initialization failed', e, stack);
+  }
 }
 
 class MainApp extends StatefulWidget {
@@ -22,3 +39,4 @@ class _MainAppState extends State<MainApp> {
     );
   }
 }
+ 
